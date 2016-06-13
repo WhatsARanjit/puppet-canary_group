bfacf3e92722b9dde003dbb4951ad8853ce97455
WhatsARanjit <whatsaranjit@gmail.com>
2016-06-13 20:33:38 +0000

    Going back to first rev

--- manifests/on.pp
+++ manifests/on.pp
@@ -3,26 +3,18 @@ application canary::on (
   String $test_env,
 ) {
 
-  case $environment {
-    'production': {
-      canary::group { $title:
-        test_node => $test_node,
-        test_env  => $test_env,
-        export    => Canary_group[$title],
-      }
-      Canary::Group produces Canary_group {}
-    }
-    default: {
-      $node_export = Canary_group[$title]
-      Canary::Node produces Canary_group {}
-    }
+  canary::group { $title:
+    test_node => $test_node,
+    test_env  => $test_env,
+    export    => Canary_group[$title],
   }
-  Canary::Node consumes Canary_group {}
+  Canary::Group produces Canary_group {}
 
   canary::node { $title:
     test_node => $test_node,
     test_env  => $test_env,
-    export    => $node_export,
     consume   => Canary_group[$title],
   }
+  Canary::Node consumes Canary_group {}
+
 }
